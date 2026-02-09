a483e322b279f8bc878f4f9b3f308ab35bddc618
Sophivorus <schenonef@gmail.com>
2026-02-08 23:14:02 -0800

    Remove Open Graph support

--- index.php
+++ index.php
@@ -7,9 +7,6 @@ $centerX = GET( 'centerX', DEFAULT_CENTERX );
 $centerY = GET( 'centerY', DEFAULT_CENTERY );
 $pixelSize = GET( 'pixelSize', DEFAULT_PIXELSIZE );
 
-$ogUrl = BASE . $centerX . '/' . $centerY . '/' . $pixelSize;
-$ogImage = BASE . "Areas?centerX=$centerX&centerY=$centerY&width=1200&height=630&pixelSize=$pixelSize&format=png";
-
 if ( is_mobile() ) {
 	include 'views/mobile.phtml';
 } else {
